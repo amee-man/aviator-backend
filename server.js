b9e05acba1917eed099ad390edf54225a0c7a937
amee-man <mulatuamanuel1@gmail.com>
2026-08-11 00:16:25 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -64,6 +64,6 @@ function runGame() {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(Server is running on port ${ PORT });
+    console.log("Server is running on port " + PORT);
     startGameLoop();
 });
